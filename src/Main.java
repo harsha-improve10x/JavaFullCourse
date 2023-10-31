9dedde9f2387161437ec353c8c0c9ae399b3b7f6
Harsha <shreeharsha.a@improve10x.com>
2023-10-31 12:33:52 +0530

    Introduction

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    // Java:- Java is an Object-oriented programming language that is used to develop
+    //        Mobile Applications, Web Applications, Desktop Applications, Games etc...
+    //
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
