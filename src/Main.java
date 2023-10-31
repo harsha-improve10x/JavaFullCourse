8d50de746ff7b2d2d8c082f2dc928d7930286438
Harsha <shreeharsha.a@improve10x.com>
2023-10-31 16:39:23 +0530

    Introduction

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
 public class Main {
-    // Java:- Java is an Object-oriented programming language that is used to develop
-    //        Mobile Applications, Web Applications, Desktop Applications, Games etc...
-    //
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
+
+ /*    Java:- Java is an Object-oriented programming language that is used to develop
+              Mobile Applications, Web Applications, Desktop Applications, Games etc...
+
+       Features of Java:-
+       1. It is easy to learn and simple to use.
+       2. It is secure, fast and powerful.
+       3. It is independent platform like Windows, Mac, Linux etc...
+
+       How Java Works?
+       First create a main method in the file and write a code
+       after that when you run the code, the process of jvm happens which means
+       it compiles the source code to the byte code, and then it interprets the
+       bytecode to the operating system.
+
+       Java Architecture:-
+       Java architecture consists of JVM, JRE and JDK.
+
+       JDK(Java Development Kit):- Java development kit is used by the developers
+       so that they can read, write, modify, and run the program.
+
+       JRE(Java Runtime Environment):- Java Runtime Environment is used to
+       read and run the program. in this you cannot edit the program.
+
+       JVM(Java Virtual Machine):- It is a process that happens during the runtime.
+       When you run the program source code compiles to bytecode and then jvm interprets
+       the bytecode to it's operating system.
+*/
